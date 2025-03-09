ea7646be34a3b3e48caae2a14958d4112f796632
unknown <matvei1065@gmail.com>
2025-03-09 23:17:11 +0300

    Final release

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <cinttypes>
 #include <algorithm>
 #include <vector>
@@ -12,22 +13,9 @@ uint64_t descedingOrder(uint64_t a){
 	for (; a > 0; a /= 10) {
 		int digit = a%10;
 		digits.push_back(digit);
-		//int z = sizeof(digits);
+	
 	}
-	/*for (size_t j = 0; j < digits.size() - 1; j++) {
-		for (int i = 0; i < digits.size()-1-j; i++) {
-			if (digits[i] < digits[i + 1]) {
-				int temp;
-				temp = digits[i];
-				digits[i] = digits[i + 1];
-				digits[i + 1] = temp;
-
-			}
-
-			
-		}
-	}
-	*/
+	
 	std::sort(digits.begin(),digits.end(),predicate );
 	for (size_t i = 0; i < digits.size(); i++) {
 		 result = result * 10 + digits[i];
@@ -36,8 +24,12 @@ uint64_t descedingOrder(uint64_t a){
 	return result;
 }
 int main() { 
+	uint64_t number;
+	std::cout << "Enter a number to sort ";
+	std::cin >> number;
+	std::cout << descedingOrder(number);
 
-	int a = descedingOrder(12345678);
+
 
 		return 0;
 }
