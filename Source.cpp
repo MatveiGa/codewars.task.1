857f16e512f44337d0fcaac09c927323b215016b
unknown <matvei1065@gmail.com>
2025-03-09 22:40:17 +0300

    Улучшил сортировку

--- Source.cpp
+++ Source.cpp
@@ -1,15 +1,20 @@
 #include <cinttypes>
-
+#include <algorithm>
 #include <vector>
+
+bool predicate(int a, int b) {
+	return a > b;
+
+}
 uint64_t descedingOrder(uint64_t a){
 	std::vector<uint64_t> digits;
-	int result = 0;
+	uint64_t result = 0;
 	for (; a > 0; a /= 10) {
 		int digit = a%10;
 		digits.push_back(digit);
 		//int z = sizeof(digits);
 	}
-	for (int j = 0; j < digits.size() - 1; j++) {
+	/*for (size_t j = 0; j < digits.size() - 1; j++) {
 		for (int i = 0; i < digits.size()-1-j; i++) {
 			if (digits[i] < digits[i + 1]) {
 				int temp;
@@ -22,8 +27,9 @@ uint64_t descedingOrder(uint64_t a){
 			
 		}
 	}
-	
-	for (int i = 0; i < digits.size(); i++) {
+	*/
+	std::sort(digits.begin(),digits.end(),predicate );
+	for (size_t i = 0; i < digits.size(); i++) {
 		 result = result * 10 + digits[i];
 		
 	}
